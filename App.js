6a04289cfef80c154d14fde1dd384c0e7e53e60f
Harsh <harsh@planetsustech.com>
2024-04-16 20:59:01 +0530

    onpress etc events , scrollview etc

--- App.js
+++ App.js
@@ -1,16 +1,77 @@
-import { Text, View,Image, ImageBackground } from "react-native";
-const logoImage = require('./assets/adaptive-icon.png');
+import { Button, Image, ScrollView, Text, View, Pressable } from "react-native";
+const logoImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const handleTextPress = () => {
+    console.log("Text onPress");
+  };
+
+  const handlePressIn = () => {
+    console.log("Pressable onPressIn");
+  };
+
+  const handlePressOut = () => {
+    console.log("Pressable onPressOut");
+  };
+
+  const handleLongPress = () => {
+    console.log("Pressable onLongPress");
+  };
+
   return (
     <View
       style={{
-        backgroundColor: "green",
+        backgroundColor: "plum",
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 60,
       }}
     >
+      <Image source={logoImage} style={{ width: 300, height: 300 }}></Image>
+      <Button
+        color="midnightblue"
+        disabled
+        title="Press"
+        onPress={() => console.log("Button pressed")}
+      ></Button>
+      <ScrollView>
+        <Pressable
+          onPress={handleTextPress}
+          onPressIn={handlePressIn}
+          onPressOut={handlePressOut}
+          onLongPress={handleLongPress}
+        >
+          <Text>
+            ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Text>
+        </Pressable>
+      </ScrollView>
       {/* <View
        style={{
          backgroundColor: "plum",
@@ -32,9 +93,9 @@ export default function App() {
       {/* <Text style={{ color:'#ffff' }}>Hello World</Text> */}
       {/* <Image source={logoImage} style={{width:300,height:300}}/>
       <Image source={{uri:"https://picsum.photos/id/237/200/300"}} style={{width:300,height:300}}/> */}
-      <ImageBackground source={logoImage}>
+      {/* <ImageBackground source={logoImage}>
         <Text>Hello hi i am new here</Text>
-      </ImageBackground>
+      </ImageBackground> */}
     </View>
   );
 }
